ec5985ee267e801dacd2f31d8ecf374dd967f172
Albert NIYONSENGA <a.niyonseng@alustudent.com>
2025-10-07 12:51:09 +0000

    fix: proper test file

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -3,22 +3,28 @@
 Unit tests for the console
 """
 import unittest
-import sys
 import os
+import sys
 
-# Adding parent directory to the path to import console
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+# Add parent directory to path to import console
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 try:
     from console import HBNBCommand
+    HAS_CONSOLE = True
 except ImportError:
-    HBNBCommand = None
+    HAS_CONSOLE = False
 
 
 class TestConsole(unittest.TestCase):
     """Test cases for HBNBCommand class"""
 
-    @unittest.skipIf(HBNBCommand is None, "Console module not implemented")
+    @unittest.skipIf(not HAS_CONSOLE, "console module not available")
+    def test_console_exists(self):
+        """Test that console can be imported"""
+        self.assertTrue(HAS_CONSOLE)
+
+    @unittest.skipIf(not HAS_CONSOLE, "console module not available")
     def test_console_initialization(self):
         """Test console initialization"""
         console = HBNBCommand()
